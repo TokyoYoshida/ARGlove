6936241efad0e2b4d5294b596d14ccc1a257509a
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-09-29 06:04:31 +0900

    first commit

---
+++ ARGlove/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARGlove
+//
+//  Created by TokyoYoshida on 2020/09/29.
+//  Copyright © 2020 TokyoMac. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}

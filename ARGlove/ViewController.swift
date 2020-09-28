1ece9e3c6e00de624c37bc3668d9a1e02d0aa3d5
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-09-29 06:07:47 +0900

    tracking

--- ARGlove/ViewController.swift
+++ ARGlove/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        let leftHandAnchor = AnchorEntity()
+        var leftHandGlove: Experience.LeftHand!
+
+        override func viewDidLoad() {
+            func loadAnchor() {
+                leftHandGlove = try! Experience.loadLeftHand()
+                arView.scene.addAnchor(leftHandAnchor)
+            }
+            super.viewDidLoad()
+            loadAnchor()
+        }
+
+        override func viewDidAppear(_ animated: Bool) {
+            func configureARKit() {
+                guard ARBodyTrackingConfiguration.isSupported else {
+                        fatalError("This feature is only supported on devices with an A12 chip")
+                }
+                arView.session.delegate = self
+
+                let configuration = ARBodyTrackingConfiguration()
+                arView.session.run(configuration)
+
+                arView.debugOptions = [.showWorldOrigin]
+            }
+            super.viewDidAppear(animated)
+            configureARKit()
+        }
     }
-}
+
+    extension ViewController: ARSessionDelegate{
+
+        func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+            for anchor in anchors {
+                guard let bodyAnchor = anchor as? ARBodyAnchor else { continue }
+                guard let leftHandTransform = bodyAnchor.skeleton.modelTransform(for: .leftHand) else { continue }
+                if leftHandGlove.parent == nil {
+                    leftHandAnchor.addChild(leftHandGlove)
+                }
+                let leftHandTrans = bodyAnchor.transform * leftHandTransform
+                leftHandAnchor.transform = Transform(matrix: leftHandTrans)
+            }
+        }
+    }
